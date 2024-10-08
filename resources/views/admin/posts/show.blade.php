e861c01b44110299fc7f0c59856f96f1646687da
EndriuK <157508097+EndriuK@users.noreply.github.com>
2024-10-08 18:03:11 +0200

    [UPD] aggiunta funzione per far apparire l'immagine di copertina

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -5,6 +5,11 @@
         <div class="row">
             <div class="col-12">
                 <h2>{{ $post->title }}</h2>
+                @if (Str::startsWith($post->cover_image, 'https'))
+                    <img class="cover_image" src="{{ $post->cover_image }}" alt="{{ $post->title }}">
+                @else
+                    <img class="cover_image" src="{{ asset('./storage/'.$post->cover_image)}}" alt="{{ $post->title }}">
+                @endif
                 <p>{{ $post->slug }}</p>
                 <p>{{ $post->content }}</p>
             </div>
